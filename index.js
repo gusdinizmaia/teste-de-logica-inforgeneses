d2cd30a6795d8c8174b4013b74cc9c3382dd82a5
gusdinizmaia <gustavodiniz37@hotmail.com>
2023-11-28 03:02:47 -0300

    feat: projeto refatorado

--- index.js
+++ index.js
@@ -22,13 +22,20 @@ function izi(){
 
 function natty(){
 
+// - Utilize a linguagem de programação de sua escolha para implementar o programa.
+// - Comente seu código para explicar os passos relevantes.
+// - Não é necessário realizar entrada de dados pelo usuário; o programa deve imprimir os números automaticamente.
+// - Verifique se a saída está correta e de acordo com a sequência de Fibonacci dada.
+
     var result = []
     var count = 0
 
+    // a criação de uma váriavel no loop vai indicar quantos numeros terão no array final
     for(var i = 0; result.length < 40; i++){
 
         result.push(count)
         
+        //observei que os numero começavam a somar com o antecessor a partir do terceiro numero por isso fiz essa condição para somar sempre com antecessor usando o i como
         if(i > 1){
             count = result[i-1] + result[i]
         }
@@ -40,126 +47,7 @@ function natty(){
     return result
 }
 
-// class fakeNatty{
 
-//     // S : sistema de pontuação,  K : ultimo a ganhar pontos, G : grandes premios,  P : pilotos
-
-//     // primeira linha => G P, ambos >= 1 e <=100
-//     // nas outras linhas sempre vão ser os pilotos e suas posições => 3, 5 (piloto 1 terceira posição, piloto 2 quinta posição)
-
-//     // linha de quebra  => S, um unico S indicando o numero de sistemas de pontuação
-//     // nas outras linhas vai ser o K k0, k1, ... , kn−1  => K indicando a ultima ordem de chegada que receberá pontos e k0, k1, ... , kn−1 indicando pontos a serem atribuidos a cada colocado
-//     // o primeiro inteiro indica os pontos do primeiro colocado e assim por diante
-
-//     // O último caso de teste é seguido por uma linha que contém apenas dois números zero separados por um espaço em branco.
-
-//     // deve imprimir o campeão mundial em cada sistema aplicado, caso haja varios campeões eles tem que ser escritos separados por espaços em ordem crescente, ex : 1 2 3 (piloto 1,2,3 campeões) 
-    
-//     constructor(){
-//     this.string = `1 3
-//     3 2 1
-//     3
-//     3 5 3 2
-//     3 5 3 1
-//     3 1 1 1
-//     3 10
-//     1 2 3 4 5 6 7 8 9 10
-//     10 1 2 3 4 5 6 7 8 9
-//     9 10 1 2 3 4 5 6 7 8
-//     2
-//     5 5 4 3 2 1
-//     3 10 5 1
-//     2 4
-//     1 3 4 2
-//     4 1 3 2
-//     2
-//     3 3 2 1
-//     3 5 4 2
-//     0 0`
-//     this.campeonatos = []
-//     this.startLine = 0
-//     this.endLine = string.indexOf('\n', startLine)
-//     this.lineValue = string.substring(startLine, endLine)
-// }
-
-//     static skipLine(){
-//         if( this.endLine === -1){
-//             this.lineValue = string.substring(this.startLine)
-//         }else{
-//             this.lineValue = string.substring(this.startLine, this.endLine)
-//         }
-
-//         this.startLine = this.endLine+5
-//         this.endLine = string.indexOf('\n',  this.startLine+1)
-//     }
-
-//     createCamp(){
-//         for(let i = 0; i < 8; i++){
-
-//             skipLine()
-
-//             let validateInfo = this.lineValue.split(' ')
-    
-//             if(!parseInt(validateInfo[0])){
-//                 return this.campeonatos
-//             }
-    
-//             if(validateInfo.length === 2 ){
-    
-//                 let premios = []
-//                 let pilotos = validateInfo[1]
-//                 let numeroDePremios = validateInfo[0]
-                
-//                 for(let i = 0; i < numeroDePremios; i++){
-                    
-//                     if(endLine === -1){
-//                         lineValue = string.substring(startLine)
-//                     }else{
-//                         lineValue = string.substring(startLine, endLine)
-//                     }
-    
-//                     startLine = endLine+5
-//                     endLine = string.indexOf('\n', startLine+1)
-    
-//                     let novoPremio = lineValue.split(' ')
-    
-//                     premios.push([...novoPremio])
-//                 }
-                
-//                 this.campeonatos.push({pilotos:pilotos, premios: premios})
-//             }
-    
-//             skipLine(lineValue, startLine, endLine)
-    
-//             validateInfo = lineValue.split(' ')
-    
-//             if(validateInfo.length === 1 ){
-//                 let sistemasDePontuação = []
-//                 let numeroDeSistemas = validateInfo[0]
-    
-//                 for(let i = 0; i < numeroDeSistemas; i++){
-//                     if(endLine === -1){
-//                         lineValue = string.substring(startLine)
-//                     }else{
-//                         lineValue = string.substring(startLine, endLine)
-//                     }
-    
-//                     startLine = endLine+5
-//                     endLine = string.indexOf('\n', startLine+1)
-    
-//                     let novoSistema = lineValue.split(' ')
-    
-//                     sistemasDePontuação.push([...novoSistema])
-//                 }
-    
-//                 this.campeonatos[campeonatos.length-1].sistemasDePontuação = sistemasDePontuação
-//             }
-//         }
-//         return this.campeonatos
-//     }
-
-
-// }
 
 function fakeNatty(){
 
@@ -224,7 +112,6 @@ function fakeNatty(){
 
         startLine = endLine+5
         endLine = string.indexOf('\n', startLine+1)
-        // skipLine(lineValue, startLine, endLine)
 
         let validateInfo = lineValue.split(' ')
 
